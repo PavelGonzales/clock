bc1bece3502fd867d63dae60fd7bcf0ff0560a02
Pavel Gonzales <p.gonzales@softpro.com>
2021-09-29 13:17:41 +0300

    release

---
+++ dist/precache-manifest.6f1b188dfd572e3c060025797f883107.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb496ed8e139c0f9cc3c",
+    "url": "/css/app.e130993f.css"
+  },
+  {
+    "revision": "e44c5423718c745ce272c029d6d834a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb496ed8e139c0f9cc3c",
+    "url": "/js/app.a8cddab6.js"
+  },
+  {
+    "revision": "72cf0d0151936b8ec85b",
+    "url": "/js/chunk-vendors.23ecf363.js"
+  },
+  {
+    "revision": "da6e173abb085b9574057dcc44c4de8b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
