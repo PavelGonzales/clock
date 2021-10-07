15f338ccd278607c37f970ddaa3b0e960f1c6bb9
Pavel Gonzales <p.gonzales@softpro.com>
2021-10-07 15:19:33 +0300

    sidebar + store

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
+import PrimeVue from 'primevue/config';
 import router from './router'
 import store from './store'
 import VueGtag from "vue-gtag";
+import 'primeflex/primeflex.css';
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .use(VueGtag, {
-    config: { id: "G-W7V2BHX7E3" }
-  })
-  .mount('#app');
+const app = createApp(App)
+app.use(store);
+app.use(router);
+app.use(PrimeVue);
+app.use(VueGtag, {
+  config: { id: "G-W7V2BHX7E3" }
+});
+app.mount('#app');
